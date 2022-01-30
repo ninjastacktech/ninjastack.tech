0eeb4a2f85a1e1cd31df1b1989dc117f8f441b41
bogdan.ivanov <bogdan.ivanov@technov.ro>
2022-01-31 00:31:56 +0200

    add v1 presentation site

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      display: ["Inconsolata", "Menlo", "monospace"],
+      body: ["Inconsolata", "Menlo", "monospace"],
+    },
+  },
+};
