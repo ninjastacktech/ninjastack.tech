ad8e4ab16ffba15f3360977afb1e9484b2d11fe7
alex.dinca <dnkalex@gmail.com>
2022-05-05 22:09:33 +0300

    opacity

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -7,7 +7,7 @@ export function MainLogo(props: IMainLogoProps) {
   return (
     <div className="flex mt-32 absolute w-full top-4">
       <div className="w-full h-full flex relative items-center justify-center text-center ">
-        <img src={circle} className="flex h-80" alt="circle" />
+        <img src={circle} className="flex h-80 opacity-80" alt="circle" />
         <img src={guy} className="flex h-[30rem] absolute" alt="circle" />
       </div>
     </div>
